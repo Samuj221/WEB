689f12667e110c5dce4091c91785690038b0fe5d
Samuj221 <samueljmejiad231@gmail.com>
2025-08-26 21:51:20 -0500

    proyecto

--- app/src/main/java/com/example/myapplication/ui/screens/CharacterListScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/CharacterListScreen.kt
@@ -3,9 +3,9 @@ package com.example.myapplication.ui.screens
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.stickyHeader
+
+import androidx.compose.foundation.lazy.*
+
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
